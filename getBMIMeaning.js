667ca2f67f5c6e423b05896de5ec912907988af4
BesttJTL <111588284+BesttJTL@users.noreply.github.com>
2022-09-09 02:42:33 +0700

    finish1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,32 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  //console.log(BMI=weight/(height*height))
+  let BMI=weight/(height*height)
+  if(BMI<=18.5){
+      //console.log('Underweight')
+      return 'Underweight'
+  }
+  else if (BMI>=18.5 && BMI<=24.9){
+      //console.log('Normal weight')
+      return 'Normal weight'
+  }
+  else{
+      //console.log('Overweight')
+      return 'Overweight'
+  }
 }
+
 function getBMIMeaning(weight, height) {
-  //code here
+  return calculateBMI(weight,height)
 }
-module.exports = getBMIMeaning
+
+
+//module.exports = getBMIMeaning
+// getBMIMeaning(65, 1.8)
+// getBMIMeaning(80, 1.7)
+// getBMIMeaning(44, 1.6)
+
+// calculateBMI(65, 1.8)
+// calculateBMI(80, 1.7)
+// calculateBMI(44, 1.6)
